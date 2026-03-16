e0f1fb684cca40d875e63f9438b02ebba6becfc8
DrwnDrDev <drwn.dr.dev@gmail.com>
2026-03-15 21:02:30 -0500

    Actualizar rutas de archivos y datos en rips_generado.json

--- Ejercicios/venv/FEV_RIPS.py
+++ Ejercicios/venv/FEV_RIPS.py
@@ -4,7 +4,7 @@ import numpy as np
 import inspect
 
 # 📥 Cargar archivo Excel
-archivo_excel = "C:\\Users\\dwndz\\OneDrive\\Escritorio\\RIPS\\Dra_Svetlana\\NEPS TATIANA-MARIA COTE.xlsx"
+archivo_excel = "C:\\Users\\dwndz\\OneDrive\\Escritorio\\RIPS\\Dr_Ordonez\\RIPS_CIRUGIA_MAGDA_SEGURA_SEP_2025.xlsx"
 xls = pd.ExcelFile(archivo_excel)
 
 # 🧩 Funciones auxiliares
